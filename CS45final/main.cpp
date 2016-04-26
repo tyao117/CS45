c0e6b0cf4769f4e92d904fc876da978069da0158
Wilsonl9 <Wilsonlam9999@gmail.com>
2016-04-26 12:29:55 -0700

    Update
    fixed everything

--- CS45final/main.cpp
+++ CS45final/main.cpp
@@ -19,7 +19,7 @@ void performOperation(vector<byte> bigNum, const char &op, vector<byte> bigNum2,
 void add(vector<byte> bigNum, vector<byte> bigNum2, vector<byte> &bigNumAns);
 void subtract(vector<byte> bigNum, vector<byte> bigNum2, vector<byte> &bigNumAns);
 void multiply(vector<byte> bigNum, vector<byte> bigNum2, vector<byte> &bigNumAns);
-void divide(const vector<byte> &bigNum, const vector<byte> &bigNum2, vector<byte> &bigNumAns, vector<byte> &bigNumAnsRemainder);
+void divide(vector<byte> bigNum, vector<byte> bigNum2, vector<byte> &bigNumAns, vector<byte> &bigNumAnsRemainder);
 void incrementUp(vector<byte> &bigNumAns);
 bool lessThan(const vector<byte> &bigNum, const vector<byte> &bigNum2);
 bool greaterThan(const vector<byte> &bigNum, const vector<byte> &bigNum2);
@@ -31,7 +31,7 @@ void factorial(vector<byte> bigNum, vector<byte> &bigNumAns);
 void decrement(vector<byte> &bigNumAns);
 void combinations(const vector<byte> &bigNum, const vector<byte> &bigNum2, vector<byte> &bigNumAns, vector<byte> &bigNumAnsRemainder);
 void permutations(const vector<byte> &bigNum, const vector<byte> &bigNum2, vector<byte> &bigNumAns, vector<byte> &bigNumAnsRemainder);
-void gcd(const vector<byte> &bigNum, const vector<byte> &bigNum2, vector<byte> &bigNumAns);
+void gcd(vector<byte> bigNum, vector<byte> bigNum2, vector<byte> &bigNumAns);
 void displayAllMemory(const vector<byte> memory[], const vector<byte> memoryRemainder[]);
 void outputNumber(ostream &out, const vector<byte> &bigNum);
 bool loadFromFile(const string &fileName, vector<byte> memory[], vector<byte> memoryRemainder[]);
@@ -48,7 +48,6 @@ int main()
     clearMemories(memory, memoryRemainder);
     while(getInput(line))
     {
-//        trim(line);
         processLine(line, memory, memoryRemainder, saved);
     }
     return 0;
@@ -69,22 +68,6 @@ bool getInput(string &line)
     return true;
 }
 
-//void trim(string &line)
-//{
-//    while(line[0] == ' ')
-//    {
-//        line.erase(0, 1);
-//    }
-//    while(line[line.size() - 1] == ' ')
-//    {
-//        line.erase(line.end());
-//    }
-//    for(int i = 0; i < line.find_first_of(" "); ++i)
-//    {
-//        line[i] = toupper(line[i]);
-//    }
-//}
-
 void processLine(const string &line, vector<byte> memory[], vector<byte> memoryRemainder[], bool &saved)
 {
     vector<byte> bigNum;
@@ -94,8 +77,17 @@ void processLine(const string &line, vector<byte> memory[], vector<byte> memoryR
     char op;
     stringstream ss;
     string action;
+    int functionFound;
     ss << line;
-    ss >> action;
+    if((functionFound = ss.str().find_first_of("(!")) < string::npos)
+    {
+        action = ss.str().substr(0, functionFound);
+        ss.ignore(functionFound);
+    }
+    else
+    {
+        ss >> action;
+    }
     if(parseNumber(action, bigNum, memory, memoryRemainder))
     {
         string parse;
@@ -105,6 +97,7 @@ void processLine(const string &line, vector<byte> memory[], vector<byte> memoryR
             if(ss.eof())
             {
                 cout << "Invalid expression.\n";
+                help();
                 return;
             }
             ss >> parse;
@@ -124,8 +117,15 @@ void processLine(const string &line, vector<byte> memory[], vector<byte> memoryR
             if(invalidInput)
             {
                 cout << "Invalid Operator or Operand.\n";
+                help();
                 return;
             }
+        }
+        if(!ss.eof())
+        {
+            cout << "Invalid expression.\n";
+            help();
+            return;
         }
         performOperation(bigNum, op, bigNum2, bigNumAns, bigNumAnsRemainder);
         cout << "Output: ";
@@ -151,6 +151,7 @@ void processLine(const string &line, vector<byte> memory[], vector<byte> memoryR
                 if(ss.eof())
                 {
                     cout << "Invalid expression.\n";
+                    help();
                     return;
                 }
                 ss >> parse;
@@ -169,14 +170,22 @@ void processLine(const string &line, vector<byte> memory[], vector<byte> memoryR
                 case 2:
                 {
                     invalidInput = !parseNumber(parse, bigNum2, memory, memoryRemainder);
+                    invalidInput = !invalidInput && op == '!';
                     break;
                 }
                 }
                 if(invalidInput)
                 {
                     cout << "Invalid Operator or Operand.\n";
+                    help();
                     return;
                 }
+            }
+            if(!ss.eof())
+            {
+                cout << "Invalid expression.\n";
+                help();
+                return;
             }
             outputNumber(cout, bigNum);
             cout << endl;
@@ -193,6 +202,18 @@ void processLine(const string &line, vector<byte> memory[], vector<byte> memoryR
             return;
         }
         return;
+    }
+    if(action == "C")
+    {
+        cout << "c";
+    }
+    if(action == "P")
+    {
+        cout << "P";
+    }
+    if(action == "GCD")
+    {
+        cout << "gcd";
     }
     if(action == "SHOW")
     {
@@ -275,7 +296,8 @@ void processLine(const string &line, vector<byte> memory[], vector<byte> memoryR
 
 void performOperation(vector<byte> bigNum, const char &op, vector<byte> bigNum2, vector<byte> &bigNumAns, vector<byte> &binNumAnsRemainder)
 {
-    
+    removeLeadingZero(bigNum);
+    removeLeadingZero(bigNum2);
     switch(op)
     {
     case '+':
@@ -296,6 +318,11 @@ void performOperation(vector<byte> bigNum, const char &op, vector<byte> bigNum2,
     case '/':
     {
         divide(bigNum, bigNum2, bigNumAns, binNumAnsRemainder);
+        break;
+    }
+    case '!':
+    {
+        factorial(bigNum, bigNumAns);
         break;
     }
     default:
@@ -347,22 +374,30 @@ bool parseOperator(const string &line, char &op)
 void add(vector<byte> bigNum, vector<byte> bigNum2, vector<byte> &bigNumAns)
 {
     bigNumAns.resize(0);
+    byte result = 0;
     byte carry = 0;
     unsigned int minSize = min(bigNum.size(), bigNum2.size());
     for(unsigned int i = 0; i < minSize; ++i)
     {
-        bigNumAns.push_back((bigNum[i] + bigNum2[i] + carry) % 10);
-        carry = (bigNum[i] + bigNum2[i]) / 10;
+        result = bigNum[i] + bigNum2[i] + carry;
+        bigNumAns.push_back(result % 10);
+        carry = result / 10;
     }
     for(unsigned int i = minSize; i < bigNum.size(); ++i)
     {
-        bigNumAns.push_back((bigNum[i] + carry) % 10);
-        carry = (bigNum[i] + carry) / 10;
+        result = bigNum[i] + carry;
+        bigNumAns.push_back(result % 10);
+        carry = result / 10;
     }
     for(unsigned int i = minSize; i < bigNum2.size(); ++i)
     {
-        bigNumAns.push_back((bigNum2[i] + carry) % 10);
-        carry = (bigNum2[i] + carry) / 10;
+        result = bigNum2[i] + carry;
+        bigNumAns.push_back(result % 10);
+        carry = result / 10;
+    }
+    if(carry)
+    {
+        bigNumAns.push_back(carry);
     }
 }
 
@@ -417,36 +452,51 @@ void multiply(vector<byte> bigNum, vector<byte> bigNum2, vector<byte> &bigNumAns
     bigNumAns.resize(0);
     byte carry = 0;
     byte total = 0;
-    if(bigNum.size() > bigNum2.size())
+    int negative = 1;
+    vector<vector<byte> > subAns;
+    if(isNegative(bigNum))
+    {
+        bigNum[bigNum.size() - 1] *= -1;
+        negative *= -1;
+    }
+    if(isNegative(bigNum2))
+    {
+        bigNum2[bigNum2.size() - 1] *= -1;
+        negative *= -1;
+    }
+    if(lessThan(bigNum, bigNum2))
     {
         vector<byte> temp = bigNum;
         bigNum = bigNum2;
         bigNum2 = temp;
     }
-    for(size_t i = 0; i < bigNum2.size(); ++i)
+    for(size_t i = 0, end = bigNum2.size(); i < end; ++i)
     {
-        total = 0;
-        for(size_t j = min(bigNum.size() - 1, i), k = min(bigNum.size() - 1, i); k > 0; --k)
+        subAns.push_back(vector<byte>());
+        for(size_t j = 0; j < bigNum.size(); ++j)
         {
-            total = bigNum[j] * bigNum2[i - k];
+            total = bigNum[j] * bigNum2[i] + carry;
+            carry = total / 10;
+            subAns[i].push_back(total % 10);
         }
-        total = total + bigNum[0] * bigNum2[i] + carry;
-        carry = total / 10;
-        bigNumAns.push_back(total % 10);
+        if(carry)
+        {
+            subAns[i].push_back(carry);
+            carry = 0;
+        }
     }
-    for(unsigned int i = 1, last = bigNum2.size() - 1; i < bigNum.size(); ++i)
+    for(size_t i = 0; i < subAns.size(); ++i)
     {
-        total = bigNum2[last] * bigNum[i] + carry % 10;
-        carry = total / 10;
-        bigNumAns.push_back(total);
+        subAns[i].insert(subAns[i].begin(), i, (char)0);
+        add(bigNumAns, subAns[i], bigNumAns);
     }
-    if(carry)
+    if(bigNumAns.size())
     {
-        bigNumAns.push_back(carry);
+        bigNumAns[bigNumAns.size() - 1] *= negative;
     }
 }
 
-void divide(const vector<byte> &bigNum, const vector<byte> &bigNum2, vector<byte> &bigNumAns, vector<byte> &bigNumAnsRemainder)
+void divide(vector<byte> bigNum, vector<byte> bigNum2, vector<byte> &bigNumAns, vector<byte> &bigNumAnsRemainder)
 {
     bigNumAns.resize(1);
     bigNumAns[0] = 0;
@@ -454,10 +504,12 @@ void divide(const vector<byte> &bigNum, const vector<byte> &bigNum2, vector<byte
     int negative = 1;
     if(isNegative(bigNum))
     {
+        bigNum[bigNum.size() - 1] *= -1;
         negative *= -1;
     }
     if(isNegative(bigNum2))
     {
+        bigNum2[bigNum2.size() - 1] *= -1;
         negative *= -1;
     }
     bigNumAnsRemainder = bigNum;
@@ -582,7 +634,7 @@ void factorial(vector<byte> bigNum, vector<byte> &bigNumAns)
     while(greaterThan(bigNum, vector<byte>(1, 1)))
     {
         decrement(bigNum);
-        multiply(bigNumAns, bigNum, bigNumAns);
+        multiply(vector<byte>(bigNumAns), bigNum, bigNumAns);
     }
 }
 
@@ -590,16 +642,22 @@ void decrement(vector<byte> &bigNumAns)
 {
     for(unsigned int i = 0, end = bigNumAns.size(); i < end; ++i)
     {
-        if(bigNumAns[i] == 0)
+        if(bigNumAns.size())
         {
-            if(i == end - 1 && bigNumAns[i] == 1)
+            if(i == bigNumAns.size() - 1 && bigNumAns[i] == 1)
             {
                 bigNumAns.pop_back();
             }
-            bigNumAns[i] = 9;
-            continue;
+            if(bigNumAns[i])
+            {
+                --bigNumAns[i];
+                break;
+            }
+            else
+            {
+                bigNumAns[i] = 9;
+            }
         }
-        --bigNumAns[i];
     }
 }
 
@@ -624,7 +682,7 @@ void permutations(const vector<byte> &bigNum, const vector<byte> &bigNum2, vecto
     divide(numerator, denominator, bigNumAns, bigNumAnsRemainder);
 }
 
-void gcd(const vector<byte> &bigNum, const vector<byte> &bigNum2, vector<byte> &bigNumAns)
+void gcd(vector<byte> bigNum, vector<byte> bigNum2, vector<byte> &bigNumAns)
 {
     vector<byte> numbers[2] = {bigNum, bigNum2};
     vector<byte> junk;
